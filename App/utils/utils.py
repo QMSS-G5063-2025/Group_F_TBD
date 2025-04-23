f21ef336c696a038b95b322f99a6fc8059d4bb99
Di Liu <dl3738@cumc.columbia.edu>
2025-04-23 12:55:53 -0400

    Add colormap comparison & log scale mapping

--- App/utils/utils.py
+++ App/utils/utils.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 from matplotlib.colors import to_hex
-from streamlit.components.v1 import html
 
 NAME_MAPPING = {
     "Residual_Chlorine": "Residual Chlorine Content (mg/L)",
@@ -11,19 +10,43 @@ NAME_MAPPING = {
     "Sample.Number": "Sample ID",
 }
 
+COLOR_BAR_SETTING = {
+    "Residual_Chlorine": {"cmap": "bwr", "min_clip": None, "max_clip": None, "log_scale_mapping": False},
+    "Turbidity": {"cmap": "bwr", "min_clip": 0.1, "max_clip": 1.5, "log_scale_mapping": False},
+}
 
-def get_color_map(vals, selected_param, cmap="OrRd", n_stops=5, max_clip=None):
-    min_val = vals.min()
+
+def get_color_map(
+    vals, selected_param, cmap="OrRd", n_stops=5, min_clip=None, max_clip=None, log_scale_mapping=False
+):
+    raw_min = vals.min()
     raw_max = vals.max()
+
+    max_label = f"≥ {max_clip:.2f}" if not max_clip is None else f"{raw_max:.2f}"
+    min_label = f"{min_clip:.2f} ≤" if not min_clip is None else f"{raw_min:.2f}"
+    val_label = f"{min_label} → {max_label}"
+
     if max_clip is None:
         max_clip = raw_max
-    clipped = np.clip(vals, min_val, max_clip)
-    norm = plt.Normalize(vmin=min_val, vmax=max_clip)
+    if min_clip is None:
+        min_clip = raw_min
+
+    if log_scale_mapping:
+        mapped = np.log(vals)
+        clip_min_mapped = np.log(min_clip)
+        clip_max_mapped = np.log(max_clip)
+    else:
+        mapped = vals.copy()
+        clip_min_mapped = min_clip
+        clip_max_mapped = max_clip
+
+    clipped = np.clip(mapped, clip_min_mapped, clip_max_mapped)
+    norm = plt.Normalize(vmin=clip_min_mapped, vmax=clip_max_mapped)
 
     cmap = plt.get_cmap(cmap)
     colors = (cmap(norm(clipped)) * 255).astype(int)
 
-    stops = np.linspace(min_val, max_clip, n_stops)
+    stops = np.linspace(clip_min_mapped, clip_max_mapped, n_stops)
     hex_colors = [to_hex(cmap(norm(v)), keep_alpha=True) for v in stops]
     gradient_css = ", ".join(hex_colors)
 
@@ -38,11 +61,6 @@ def get_color_map(vals, selected_param, cmap="OrRd", n_stops=5, max_clip=None):
         "background-color: rgba(0,0,0,0.6); padding:2px 6px; "
         "border-radius:3px; text-shadow:1px 1px 2px rgba(0,0,0,0.7);"
     )
-
-    if max_clip < raw_max:
-        val_label = f"{min_val:.2f} → ≥{max_clip:.2f}"
-    else:
-        val_label = f"{min_val:.2f} → {max_clip:.2f}"
 
     html = f"""
     <div style="display:flex; align-items:center; margin-top:20px;">
