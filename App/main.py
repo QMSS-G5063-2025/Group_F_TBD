ec13d62946f955c3f60d711dde063bdec5b1d36d
aziz-zafar <azafar@colgate.edu>
2025-04-23 15:44:33 -0400

    modified:   App/main.py

--- App/main.py
+++ App/main.py
@@ -17,13 +17,14 @@ sys.path.append(BASE_DIR)
 
 from home import render_home
 from sec1 import render_sec1
+from sec2 import render_sec2
 
 ########################################
 # layout
 ########################################
 with st.sidebar:
     st.title("Explorer")
-    sections = {"Home": "🏠", "Water Quality Change over Time": "📈", "Section2": "📊", "Section3": "📋"}
+    sections = {"Home": "🏠", "Water Quality Change over Time": "📈", "Water Quality and Health": "📊", "Section3": "📋"}
 
     for section, icon in sections.items():
         if st.button(f"{icon} {section}"):
@@ -33,3 +34,5 @@ if st.session_state.current_section == "Home":
     render_home()
 elif st.session_state.current_section == "Water Quality Change over Time":
     render_sec1()
+elif st.session_state.current_section == "Water Quality and Health":
+    render_sec2()
